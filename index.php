53c98e6182aabaa6274fd9e7fec7457e1ae3c724
Trelanor <trelanor@gmail.com>
2018-05-04 16:54:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<?php
+
+/*intégration de TWIG et de la page fonctions.php*/
+
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/php/functions.php';
+
+define('ROOT_FOLDER', 'root');
+
+$twig = new Twig_Environment(new Twig_Loader_Filesystem('./templates'), [
+
+    'cache' => false,
+    'debug' => true
+
+]);
+
+/*fonction changement d'affichage */
+
+if (empty($_GET)) {
+
+  echo $twig->render('simple.html', getHome());
+
+} else {
+
+  if (isset($_GET['view'])) {
+
+    $view = $_GET['view'];
+
+    switch($view) {
+
+      case 'simple':
+        $template = 'simple';
+        break;
+
+      case 'details':
+        $template = 'details';
+        break;
+
+      default:
+        $template = 'error';
+        break;
+
+    }
+
+    echo $twig->render($template.'.html', getData($view));
+
+  }
+
+}
+
+?>
